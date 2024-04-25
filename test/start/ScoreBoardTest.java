0e049802939c67ab018c80ad7691ff787a81caa1
kang <xjfwn@naver.com>
2024-04-26 04:12:47 +0900

    test 코드 start부분

---
+++ test/start/ScoreBoardTest.java
@@ -0,0 +1,131 @@
+package start;
+
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ScoreBoardTest extends ApplicationTest {
+    private Stage stage;
+    private ScoreBoard scoreBoard;
+    private int score;
+    private String name;
+    private String mode;
+
+    @Before
+    public void setUp() {
+        scoreBoard = new ScoreBoard();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+    }
+
+    @Test
+    public void testCreateScene() {
+        // Given
+        String expectedMode1 = "Standard Mode";
+        String expectedMode2 = "Item Mode";
+
+        // When
+        Scene scene = scoreBoard.createScene(stage);
+        GridPane root = (GridPane) scene.getRoot();
+        VBox scoreBox1 = null;
+        VBox scoreBox2 = null;
+        Button backButton = null;
+        for (Node node : root.getChildren()) {
+            if (GridPane.getColumnIndex(node) == 0 && node instanceof VBox) {
+                scoreBox1 = (VBox) node;
+            } else if (GridPane.getColumnIndex(node) == 1 && node instanceof VBox) {
+                scoreBox2 = (VBox) node;
+            } else if (GridPane.getColumnIndex(node) == 0 && node instanceof Button) {
+                backButton = (Button) node;
+            }
+        }
+
+        // Then
+        assertNotNull(scoreBox1);
+        assertNotNull(scoreBox2);
+        assertNotNull(backButton);
+        assertTrue(scoreBox1.getChildren().stream()
+                .anyMatch(child -> child instanceof Text && ((Text) child).getText().equals(expectedMode1)));
+        assertTrue(scoreBox2.getChildren().stream()
+                .anyMatch(child -> child instanceof Text && ((Text) child).getText().equals(expectedMode2)));
+    }
+    
+    @Test
+    public void testCreateScene_2() {
+        // Given
+        String expectedMode1 = "Standard Mode";
+        String expectedMode2 = "Item Mode";
+        
+        mode=expectedMode1;
+        name="JuHyeok";
+        score=1000000;
+        
+        //주의 테스트를 하면 실제로 test.txt에 반영이 됨
+        AtomicBoolean flag = new AtomicBoolean(false); // flag 변수를 AtomicBoolean으로 선언
+        scoreBoard.addScoreToFile(name,score,mode);//테스트로 위에 주어진 이름과 기록점수를 게임 종료시 입력받았을 때 처럼 호출
+        
+        // When
+        Scene scene = scoreBoard.createScene(stage,score,name,mode);
+        GridPane root = (GridPane) scene.getRoot();
+        VBox scoreBox1 = null;
+        VBox scoreBox2 = null;
+        Button backButton = null;
+        Button exitButton = null;
+        
+        
+        for (Node node : root.getChildren()) {
+            if (GridPane.getColumnIndex(node) == 0 && node instanceof VBox) {
+                scoreBox1 = (VBox) node;
+            } else if (GridPane.getColumnIndex(node) == 1 && node instanceof VBox) {
+                scoreBox2 = (VBox) node;
+            } else if (GridPane.getColumnIndex(node) == 0 && node instanceof Button) {
+                backButton = (Button) node;
+            } else if (GridPane.getColumnIndex(node) == 1 && node instanceof Button) {
+                exitButton = (Button) node;
+            }
+        }
+        
+        // Then
+        assertNotNull(scoreBox1);
+        assertNotNull(scoreBox2);
+        assertNotNull(backButton);
+        assertNotNull(exitButton);
+        assertTrue(scoreBox1.getChildren().stream()
+                .anyMatch(child -> child instanceof Text && ((Text) child).getText().equals(expectedMode1)));
+        assertTrue(scoreBox2.getChildren().stream()
+                .anyMatch(child -> child instanceof Text && ((Text) child).getText().equals(expectedMode2)));
+        
+
+        // scoreBox1 내의 모든 요소를 테스트하는 코드 -> 새로운 신기록 빨간색으로 나타나는지 확인
+        scoreBox1.getChildren().forEach(child -> {//테스트 입력에서 standard모드였음 ->scoreBox1
+            if (child instanceof Text) {
+            	Text text = (Text) child;
+                Paint fill = text.getFill(); // Paint 객체 가져오기
+                if (fill instanceof Color) {
+                    Color color = (Color) fill; // Paint 객체를 Color로 캐스트
+                    if(color.equals(Color.RED)) {
+                    	flag.set(true); // 텍스트의 색상이 하나라도 빨간색인 경우(신기록 세운 게 빨간색으로 알맞게 되었다면) flag를 true로 설정
+                    } 
+                }
+            } 
+        });
+        assertTrue(flag.get());
+    }
+}
---
+++ test/start/StartMenuTest.java
@@ -0,0 +1,87 @@
+package start;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StartMenuTest extends ApplicationTest {
+
+    private StartMenu startMenu;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        startMenu = new StartMenu();
+        Scene scene = startMenu.createScene(stage);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        // StartMenu 인스턴스를 생성하기 위해 start() 메서드가 먼저 호출되도록 설정
+        launch(StartMenu.class);
+    }
+
+    @Test
+    public void testCreateSceneNotNull() {
+        assertNotNull(startMenu.scene);
+    }
+    @Test
+    public void testCreateSceneSize() {
+    	System.out.print(startMenu.scene.getWidth());
+        assertEquals(StartMenu.XSIZE, startMenu.scene.getWidth(), 0);
+        assertEquals(StartMenu.YSIZE, startMenu.scene.getHeight(), 0);
+    }
+
+    @Test
+    public void testNavigateMenu() {
+        // 초기 selectedIndex는 0이므로, 1을 전달하여 메뉴를 아래로 이동시켜서 selectedIndex가 1이 되도록 함
+        startMenu.navigateMenu(1);
+        assertEquals(1, startMenu.selectedIndex);
+        
+        // 다시 1을 전달하여 selectedIndex가 2가 되도록 함
+        startMenu.navigateMenu(1);
+        assertEquals(2, startMenu.selectedIndex);
+        
+        // 음수를 전달하여 메뉴를 위로 이동시켜서 selectedIndex가 1이 되도록 함
+        startMenu.navigateMenu(-1);
+        assertEquals(1, startMenu.selectedIndex);
+    }
+
+    @Test
+    public void testUpdateMenu() {
+        // 테스트를 위해 selectedIndex를 2로 설정
+        startMenu.selectedIndex = 2;
+        startMenu.updateMenu();
+        
+        // selectedIndex에 해당하는 메뉴가 빨간색으로 변경되었는지 확인
+        assertEquals("Standard Game", startMenu.menuItems[0].getText());
+        assertEquals("Item Mode Game", startMenu.menuItems[1].getText());
+        assertEquals("Setting", startMenu.menuItems[2].getText());
+        assertEquals("Score Board", startMenu.menuItems[3].getText());
+        assertEquals("Exit", startMenu.menuItems[4].getText());
+        assertEquals("Setting", startMenu.menuItems[2].getText());
+        assertEquals("Exit", startMenu.menuItems[4].getText());
+        //assertEquals("red", startMenu.menuItems[2].getFill().toString());
+        
+        // selectedIndex를 0으로 변경하여 해당 메뉴가 검정색인지 확인
+        startMenu.selectedIndex = 0;
+        startMenu.updateMenu();
+        assertEquals("Standard Game", startMenu.menuItems[0].getText());
+        assertEquals("Item Mode Game", startMenu.menuItems[1].getText());
+        assertEquals("Setting", startMenu.menuItems[2].getText());
+        assertEquals("Score Board", startMenu.menuItems[3].getText());
+        assertEquals("Exit", startMenu.menuItems[4].getText());
+        assertEquals("Setting", startMenu.menuItems[2].getText());
+        assertEquals("Exit", startMenu.menuItems[4].getText());
+        //assertEquals("black", startMenu.menuItems[0].getFill().toString());
+    }
+}
