b299f1fa8d4c0b5a7db1ca1487769e2385e1544f
githyj-jang <111761701+githyj-jang@users.noreply.github.com>
2024-04-26 04:31:48 +0900

    Block, RandomFunction, Tetris test 코드입니다

---
+++ test/AllTestsSuite.java
@@ -0,0 +1,8 @@
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"blocks", "tetris"})
+public class AllTestsSuite {
+    // 이 클래스는 테스트 스위트의 설정을 위한 것으로, 별도의 구현 코드는 필요하지 않습니다.
+}
